351e90439f7275c5054584035cfdbe3cd429e458
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2022-10-02 12:25:28 +0530

    final

--- intelligent_search.py
+++ intelligent_search.py
@@ -10,19 +10,21 @@ with open("final_all_short_summary2.pickle","rb") as f:
 
 with open("final_conclusion_summary2.pickle","rb") as f1:
     set2=pickle.load(f1)
-summary_data=[]
+summary_data={}
 for i,j in set1.items():
     summary=j+". "+set2.get(i)
-    summary_data.append(summary)
+    summary_data.update({i:summary})
+
 '''
 with open("data/vol7.json", "r") as f:
     data = json.load(f)["descriptions"]
 print(data)
 '''
+values_summary=summary_data.values()
 
 txtai_data = []
 i=0
-for text in summary_data:
+for text in values_summary:
     txtai_data.append((i, text, None))
     i=i+1
 txtai_data[0]
@@ -30,8 +32,24 @@ embeddings.index(txtai_data)
 with open("embeddings.pickle","wb") as f:
     pickle.dump(embeddings,f)
 res = embeddings.search("CII line dominant FIR emission line Galaxy primary coolant", 10)
+keys=list(summary_data.keys())
+values=list(summary_data.values())
+values=list()
+
+values_summary=list(values_summary)
 for r in res:
-    print(f"Text: {summary_data[r[0]]}")
+
+    print(f"Text: {values_summary[r[0]]}")
+    k=0
+    m=0
+    for i in values:
+        
+        if i==summary_data[r[0]]:
+            m=k
+            break
+        k+=1
+    print(keys[m])
+
     print(f"Similarity: {r[1]}")
     print()
     break
