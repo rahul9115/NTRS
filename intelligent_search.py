d0a296b6ba9b55923595286b47796ae52024b6d6
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2022-10-02 11:47:32 +0530

    done

--- intelligent_search.py
+++ intelligent_search.py
@@ -1,9 +1,37 @@
 from txtai.embeddings import Embeddings
 import json
+import pickle
 embeddings = Embeddings({
     
     "path": "sentence-transformers/all-MiniLM-L6-v2"
 })
+with open("final_all_short_summary2.pickle","rb") as f:
+    set1=pickle.load(f)
+
+with open("final_conclusion_summary2.pickle","rb") as f1:
+    set2=pickle.load(f1)
+summary_data=[]
+for i,j in set1.items():
+    summary=j+". "+set2.get(i)
+    summary_data.append(summary)
+'''
 with open("data/vol7.json", "r") as f:
     data = json.load(f)["descriptions"]
-len(data)
+print(data)
+'''
+
+txtai_data = []
+i=0
+for text in summary_data:
+    txtai_data.append((i, text, None))
+    i=i+1
+txtai_data[0]
+embeddings.index(txtai_data)
+with open("embeddings.pickle","wb") as f:
+    pickle.dump(embeddings,f)
+res = embeddings.search("CII line dominant FIR emission line Galaxy primary coolant", 10)
+for r in res:
+    print(f"Text: {summary_data[r[0]]}")
+    print(f"Similarity: {r[1]}")
+    print()
+    break
